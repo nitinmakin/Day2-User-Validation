35689fa0945a137a7361c73d43a546a69dbb81e7
nitinmakin <nitinmakin124.com>
2020-08-29 00:47:24 +0530

    Added-Checking email validation

--- UserValidation.java
+++ UserValidation.java
@@ -4,35 +4,42 @@ import java.util.regex.*;
 
 public class UserValidation
 {
-        private Pattern pattern;
-        private Matcher matcher;
-        private  String firstName = "^[A-Z]([a-z]{2,15})*";
-        private String lastName = firstName;
-       // private String eMail = " ";
-        Scanner sn=new Scanner(System.in);
+    private Pattern pattern;
+    private Matcher matcher;
+    private  String firstName = "^[A-Z]([a-z]{2,15})*";
+    private String lastName = firstName;
+    private String eMail = "^[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.]co(m|.in)$";
+    Scanner sn=new Scanner(System.in);
 
-        public UserValidation()
-        {
-            pattern=pattern.compile(firstName);
-          //  pattern=pattern.compile(lastName);
-        }
+    public UserValidation()
+    {
+        pattern = pattern.compile(firstName);
+        pattern = pattern.compile(lastName);
+        pattern =pattern.compile(eMail);
+    }
 
-        public  boolean validateFirstName(String firstName)
-        {
-            matcher=pattern.matcher(firstName);
-            return matcher.matches();
-        }
-        public  boolean validateLastName(String LastName)
-        {
-            matcher=pattern.matcher(lastName);
-            return matcher.matches();
-        }
-//
+    public  boolean validateFirstName(String firstName)
+    {
+        matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+    public  boolean validateLastName(String LastName)
+    {
+        matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
+    public boolean validateEMail(String eMail)
+    {
+        matcher = pattern.matcher(eMail);
+        return matcher.matches();
+    }
+
+
 //        public static void main(String[] args)
 //        {
 //            UserValidation uservalidation = new UserValidation();
-//            System.out.println(uservalidation.validateFirstName("Nit"));
+//            System.out.println(uservalidation.validateEMail("nitinmakin124@gmail.com"));
 //        }
-    }
+}
 
 
